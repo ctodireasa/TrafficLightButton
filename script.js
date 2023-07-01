2a4ecafa86f858ef384681b46d91a173e21960bc
ctodireasa <138281170+ctodireasa@users.noreply.github.com>
2023-07-01 09:45:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function changingColorsonTrafficLights() {
+  let traficLights = document.getElementById("nextColor").style.backgroundColor
+  if (traficLights === 'red') {
+      document.getElementById("nextColor").style.backgroundColor = 'yellow'
+  } else if (traficLights === 'yellow') {
+      document.getElementById("nextColor").style.backgroundColor = 'green'
+  } else {
+      document.getElementById("nextColor").style.backgroundColor = 'red'
+  }
+}
